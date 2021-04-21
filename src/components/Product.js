683f70c97469c5ceebd8b2f5275b71da05bb5f74
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-04-21 19:31:10 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,18 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Product extends React.Component {
-  
   render() {
-    return (
-      <h2>{this.props.name}</h2>
-      <p>Producer: {this.props.producer}</p>
-    );
+    return(
+      <ul>
+        <li>Name: {this.props.name}</li>
+        <li>Producer: {this.props.producer}</li>
+        <li>Watermark? {this.props.hasWatermark.toString()}</li>
+        <li>Color: {this.props.color}</li>
+        <li>Weight: {this.props.weight}</li>
+      </ul>
+    )
   }
 }
 
-Product.propTypes = {
-  name: PropType.string.isRequired,
-  hasWatermakr: PropType.bool,
-  color: PropType.string.isRequired,
-  weight: PropType.string.isRequired
+Product.defaultProps = {
+  hasWatermark: false
 }
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermakr: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: weightRange
+}
+
+function weightRange(props, propName, componentName) {
+  componentName = componentName || 'ANONYMOUS';
+  if(props[propName]) {
+    let value = props[propName]
+    if (typeof value === 'number') {
+      return (value >= 80 && value <= 300) ? null : new Error(propName + ' in ' + componentName + " is not withing 80 and 300")
+    } else {
+      return (new Error(propName + ' in ' + componentName + " is not a number"))
+    }
+  } else {
+    return (new Error(propName + ' in ' + componentName + " is required"))
+  }
+}
+
+export default Product;
