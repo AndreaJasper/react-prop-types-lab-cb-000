52d4bd33b1be7cd408df18a2815d5fb1efb3bd61
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-04-21 19:29:09 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,18 @@
-// Code Product Component Here
+import React from 'react';
+
+class Product extends React.Component {
+  
+  render() {
+    return (
+      <h2>{this.props.name}</h2>
+      <p>Producer: {this.props.producer}</p>
+    );
+  }
+}
+
+Product.propTypes = {
+  name: PropType.string.isRequired,
+  hasWatermakr: PropType.bool,
+  color: PropType.string.isRequired,
+  weight: PropType.string.isRequired
+}
